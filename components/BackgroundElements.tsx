9f31699aed3c878ef818c854b15fffe7a8bfbe1b
ItzApipAjalah <sayangemak011111@gmail.com>
2025-12-20 02:04:49 +0700

    Initial commit: background

--- components/BackgroundElements.tsx
+++ components/BackgroundElements.tsx
@@ -1,3 +1,5 @@
+import LiquidEther from './LiquidEther';
+
 export default function BackgroundElements() {
     return (
         <>
@@ -7,6 +9,25 @@ export default function BackgroundElements() {
                 <div className="orb orb-1"></div>
                 <div className="orb orb-2"></div>
                 <div className="orb orb-3"></div>
+            </div>
+            <div style={{ position: 'fixed', inset: 0, width: '100%', height: '100%', zIndex: -1 }}>
+                <LiquidEther
+                    colors={['#5227FF', '#FF9FFC', '#B19EEF']}
+                    mouseForce={20}
+                    cursorSize={100}
+                    isViscous={false}
+                    viscous={30}
+                    iterationsViscous={32}
+                    iterationsPoisson={32}
+                    resolution={0.5}
+                    isBounce={false}
+                    autoDemo={true}
+                    autoSpeed={0.5}
+                    autoIntensity={2.2}
+                    takeoverDuration={0.25}
+                    autoResumeDelay={3000}
+                    autoRampDuration={0.6}
+                />
             </div>
         </>
     );
